8a96dbb890dfeef99ab7fe1fb6900cda24d53446
Bjarte Mehus Sunde <bms_mok@hotmail.com>
2015-01-14 15:23:11 +0100

    Fått kontakt med stockfish

--- SjakkGUI/MainWindow.xaml.cs
+++ SjakkGUI/MainWindow.xaml.cs
@@ -15,14 +15,33 @@ using System.Windows.Shapes;
 
 namespace SjakkGUI
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        UCI minSjakkMotor;
+        string motorSti = @"C:\Users\Bjarte\Documents\GitHub\Sjakk\stockfish_14053109_32bit.exe";
+
         public MainWindow()
         {
+            minSjakkMotor = new UCI();
+            minSjakkMotor.InitEngine(motorSti,"");
+          
+
             InitializeComponent();
         }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            minSjakkMotor.EngineCommand("go");
+          //  tbConsol.Text = minSjakkMotor.GetEngineOutput();
+        }
+
+
     }
 }
+
+
