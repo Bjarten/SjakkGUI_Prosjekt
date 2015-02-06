c41fff1046d4147254b89c29bb908706bf25b647
Bjarte Mehus Sunde <bms_mok@hotmail.com>
2015-02-06 15:58:57 +0100

    Har lagt til enkel kommunikasjon mot ABB robot

--- SjakkGUI/MainWindow.xaml.cs
+++ SjakkGUI/MainWindow.xaml.cs
@@ -15,9 +15,16 @@ using System.Windows.Shapes;
 using System.Threading;
 using System.Diagnostics;
 
+using ABB.Robotics;
+
+using ABB.Robotics.Controllers;
+using ABB.Robotics.Controllers.Discovery;
+using ABB.Robotics.Controllers.RapidDomain;
+
+
 namespace SjakkGUI
 {
-  
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -29,21 +36,72 @@ namespace SjakkGUI
         string motorSti = @"C:\Users\Bjarte\Documents\GitHub\Sjakk\stockfish_14053109_32bit.exe";
         Thread traad;
 
+        static string sysID = string.Empty;
+        static Controller dynamicController;
+        static NetworkScanner scanner;
+        static Controller controller;
+        static ABB.Robotics.Controllers.RapidDomain.Task[] tasks;
+        static NetworkWatcher networkwatcher;
+        static Mastership master;
+
+
         public MainWindow()
         {
+            InitializeComponent();
+
             ewh = new EventWaitHandle(false, EventResetMode.AutoReset);
             traad = new Thread(sjakkBeregninger);
             traad.IsBackground = true;
             traad.Start();
             minSjakkMotor = new UCI();
-            minSjakkMotor.InitEngine(motorSti,"");
+            minSjakkMotor.InitEngine(motorSti, "");
             minSjakkMotor.Depth = "20";
 
+            lblMode.Content = "";
 
-            // start new game
-            InitializeComponent();
+
+            CreateController();
+
+            controller.OperatingModeChanged += controller_OperatingModeChanged;
+
         }
 
+        private void CreateController()
+        {
+            scanner = new NetworkScanner();
+
+            ControllerInfo[] controllers = scanner.GetControllers(NetworkScannerSearchCriterias.Virtual);
+            controller = ControllerFactory.CreateFrom(controllers[0]);
+            controller.Logon(UserInfo.DefaultUser);
+
+            foreach (ControllerInfo controllerInfo in controllers)
+            {
+                this.lstControllerInformation.Items.Add(new ControllerInformationItems
+                {
+                    IPadress = controllerInfo.IPAddress.ToString(),
+                    ID = controllerInfo.Id,
+                    Availabillity = controllerInfo.Availability.ToString(),
+                    Virtual = controllerInfo.IsVirtual.ToString(),
+                    SystemName = controllerInfo.SystemName,
+                    Version = controllerInfo.Version.ToString(),
+                    ControllerName = controllerInfo.Name,
+                    RobotWare = ""
+                });
+            }
+
+
+            sysID = controllers[0].SystemId.ToString();
+            controller = ControllerFactory.CreateFrom(controllers[0]);
+            controller.Logon(UserInfo.DefaultUser);
+        }
+
+        void controller_OperatingModeChanged(object sender, OperatingModeChangeEventArgs e)
+        {
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                lblMode.Content = e.NewMode;
+            }));
+        }
 
         private void btnSend_Click(object sender, RoutedEventArgs e)
         {
@@ -57,21 +115,82 @@ namespace SjakkGUI
             {
                 ewh.WaitOne();
 
-                  this.Dispatcher.Invoke((Action)(() =>
-                            {
-                                minSjakkMotor.EngineCommandMove(tbNextMove.Text);
-                        }));
+                this.Dispatcher.Invoke((Action)(() =>
+                          {
+                              minSjakkMotor.EngineCommandMove(tbNextMove.Text);
+                          }));
 
                 // Venter på kalkulasjoner
                 minSjakkMotor.ewhCalculating.WaitOne();
 
-                    this.Dispatcher.Invoke((Action)(() =>
-                    {
-                        tbConsole.Clear();
-                        tbConsole.Text = "Tidligere trekk: " + minSjakkMotor.EarlierMoves + "\n" + "Beste trekk: "+ minSjakkMotor.BestMove;
-                    }));
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+                    tbConsole.Clear();
+                    tbConsole.Text = "Tidligere trekk: " + minSjakkMotor.EarlierMoves + "\n" + "Beste trekk: " + minSjakkMotor.BestMove;
+                }));
             }
         }
+
+
+        public class ControllerInformationItems
+        {
+            public string IPadress { get; set; }
+            public string ID { get; set; }
+            public string Availabillity { get; set; }
+            public string Virtual { get; set; }
+            public string SystemName { get; set; }
+            public string RobotWare { get; set; }
+            public string Version { get; set; }
+            public string ControllerName { get; set; }
+        }
+
+        private void lstControllerInformation_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+ 
+
+        }
+
+        private void btnStartRAPID_Click(object sender, RoutedEventArgs e)
+        {
+            //declare a variable of data type RapidDomain.Bool
+            ABB.Robotics.Controllers.RapidDomain.Bool rapidBool;
+
+            ABB.Robotics.Controllers.RapidDomain.RapidData rd = controller.Rapid.GetRapidData("T_ROB1", "stableSnus","flag");
+            //test that data type is correct before cast
+            if (rd.Value is ABB.Robotics.Controllers.RapidDomain.Bool)
+            {
+                rapidBool = (ABB.Robotics.Controllers.RapidDomain.Bool)rd.Value;
+                //assign the value of the RAPID data to a local variable
+                bool boolValue = rapidBool.Value;
+
+                rapidBool.Value = false;
+                //Request mastership of Rapid before writing to the controller
+                master = Mastership.Request(controller.Rapid);
+                //Change: controller is repaced by aController
+                rd.Value = rapidBool;
+                //Release mastership as soon as possible
+                master.ReleaseOnDispose = true;
+                master.Dispose();
+
+            }
+
+            //if (controller.OperatingMode == ControllerOperatingMode.Auto)
+            //{
+            //    tasks = controller.Rapid.GetTasks();
+            //    using (Mastership m = Mastership.Request(controller.Rapid))
+            //    {
+            //        //Perform operation
+            //        //tasks[0].Start();
+            //        tasks[0].Stop();
+            //    }
+            //}
+            //else
+            //{
+            //  }
+
+
+        }
+
     }
 }
 
